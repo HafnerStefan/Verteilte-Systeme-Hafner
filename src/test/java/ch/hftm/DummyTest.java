c5453e83076d1468271763573f337e4ba6c98132
Stefan Hafner <stefan.hafner@webapps.ch>
2024-09-01 18:42:48 +0200

    add Integrations Test

---
+++ src/test/java/ch/hftm/DummyTest.java
@@ -0,0 +1,13 @@
+package ch.hftm;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class DummyTest {
+
+	@Test
+	public void dummyTest() {
+
+	}
+}
